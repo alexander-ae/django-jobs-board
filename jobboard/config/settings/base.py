0de35df8203dc23181e2b97bb369c9669da6e63e
__alexander__ <alexander.ayasca.esquives@gmail.com>
2017-07-04 18:08:46 -0500

    Estructura inicial del proyecto

---
+++ jobboard/config/settings/base.py
@@ -0,0 +1,112 @@
+"""
+Django settings for jobboard project.
+"""
+
+import os
+import json
+
+
+def get_env(base_dir):
+    """ Obtenemos las 'variables de entorno' desde un archivo json."""
+    with open(os.path.join(base_dir, 'config/settings/settings.json')) as f:
+        env = json.load(f)
+        return env
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+ENV = get_env(BASE_DIR)
+SECRET_KEY = ENV.get('SECRET_KEY')
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'config.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'config.wsgi.application'
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': ENV.get('DATABASE_NAME'),
+        'HOST': ENV.get('DATABASE_HOST'),
+        'USER': ENV.get('DATABASE_USER'),
+        'PASSWORD': ENV.get('DATABASE_PASSWORD'),
+    }
+}
+
+EMAIL_HOST = ENV.get('EMAIL_HOST')
+EMAIL_HOST_USER = ENV.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = ENV.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = ENV.get('DEFAULT_FROM_EMAIL')
+SERVER_EMAIL = ENV.get('SERVER_EMAIL')
+
+# Password validation
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+# Internationalization
+LANGUAGE_CODE = 'es'
+TIME_ZONE = 'America/Lima'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+# Static and media
+
+STATIC_URL = '/static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
